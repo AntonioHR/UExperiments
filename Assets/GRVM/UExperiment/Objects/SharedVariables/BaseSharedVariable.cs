107619ea266d258102abae601e47edae971aed63
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2018-03-01 18:11:26 -0300

    Added Counters

--- Assets/GRVM/UExperiment/Objects/SharedVariables/BaseSharedVariable.cs
+++ Assets/GRVM/UExperiment/Objects/SharedVariables/BaseSharedVariable.cs
@@ -35,4 +35,8 @@ namespace GRVM.UExperiment.Objects.SharedVariables
     {
 
     }
+    public abstract class IntSharedVariable : BaseSharedVariable<int>
+    {
+
+    }
 }
---
+++ Assets/GRVM/UExperiment/Objects/SharedVariables/SimpleCounterVariable.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GRVM.UExperiment.Objects.SharedVariables
+{
+    [CreateAssetMenu]
+    public class SimpleCounterVariable : IntSharedVariable
+    {
+        public int startingValue;
+
+        public List<CounterTrigger> triggers;
+
+        protected override int StartValue
+        {
+            get
+            {
+                return startingValue;
+            }
+        }
+
+        public override int Value
+        {
+            get
+            {
+                return base.Value;
+            }
+
+            set
+            {
+                base.Value = value;
+                foreach (var t in triggers)
+                {
+                    t.Check(value);
+                }
+            }
+        }
+
+        public void AddOne()
+        {
+            Value++;
+        }
+        public void SubtractOne()
+        {
+            Value--;
+        }
+
+
+        public void Add(int change)
+        {
+            Value += change;
+        }
+        public void Subtract(int change)
+        {
+            Value -= change;
+        }
+
+
+        [Serializable]
+        public class CounterTrigger
+        {
+            public SharedEvent ev;
+            public Comparison comp;
+            public int value;
+
+            public void Check(int currentValue)
+            {
+                if(IsTrueFor(currentValue))
+                {
+                    ev.Fire();
+                }
+            }
+
+            public bool IsTrueFor(int currentValue)
+            {
+                switch (comp)
+                {
+                    case Comparison.GreaterThan:
+                        return currentValue > value;
+                    case Comparison.Equal:
+                        return currentValue == value;
+                    case Comparison.LessThan:
+                        return currentValue < value;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+
+            public enum Comparison
+            {
+                GreaterThan,
+                Equal,
+                LessThan
+            }
+        }
+    }
+}

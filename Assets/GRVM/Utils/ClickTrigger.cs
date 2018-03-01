d8046f079cf2f005821ea16cb90e98c140cbeccd
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2018-03-01 17:57:50 -0300

    Added Accuracy Example

---
+++ Assets/GRVM/Utils/ClickTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class ClickTrigger : MonoBehaviour {
+    Collider col;
+
+    public UnityEvent ev;
+
+
+	// Use this for initialization
+	void Start () {
+        col = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckMouseRaycast();
+        }
+    }
+
+    private void CheckMouseRaycast()
+    {
+        RaycastHit result;
+        if (col.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out result, float.PositiveInfinity))
+        {
+            ev.Invoke();
+        }
+    }
+}
